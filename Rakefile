748f88eb3ca5b322f4c7d7a0264d162c569728f8
Elena Resch <eli.resch@web.de>
2015-09-10 16:25:28 +0200

    Add Rakefile and definition of Connection

---
+++ Rakefile
@@ -0,0 +1,7 @@
+task :default do
+    puts "compile file"
+end
+
+task :run do
+    puts "run file"
+end
---
+++ lib/libuosqldb.rb
@@ -0,0 +1,29 @@
+# lib for uosql database connection
+
+# todo class for connection
+
+class Connection
+
+=begin
+    def connect()
+
+    def ping()
+
+    def quit()
+
+    def execute()
+
+    def get_version()
+
+    def get_address()
+
+    def get_port()
+
+    def get_username()
+
+    def get_greeting_msg()
+=end
+
+
+
+end
