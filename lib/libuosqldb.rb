78e22bbb1fa0bade64446b3c11c0fb328b7be86a
Elena Resch <eli.resch@web.de>
2015-09-13 14:59:59 +0200

    Restructure Connection class, add getter fuctions
    Use hashes for command and package types instead of classes.
    Remove most print commands.
    Add getter functions.

--- lib/libuosqldb.rb
+++ lib/libuosqldb.rb
@@ -1,81 +1,29 @@
 # lib for uosql database connection
 require 'socket'
 
-QUIT = 0
-PING = 1
-
-GREET = 0
-LOGIN = 1
-COMMAND = 2
-ERROR = 3
-OK = 4
-ACCDENIED = 6
-ACCGRANTED = 7
-
-PkgType = Hash.new()
-PkgType[GREET] = [0,0,0,0]
-PkgType[LOGIN] = [0,0,0,1]
-PkgType[COMMAND] = [0,0,0,2]
-PkgType[ERROR] = [0,0,0,3]
-PkgType[OK] = [0,0,0,4]
-PkgType[ACCDENIED] = [0,0,0,6]
-PkgType[ACCGRANTED] = [0,0,0,7]
-
-class Login 
-# """ Doc comments """
-    attr_reader :username
-    attr_reader :password
-
-    def initialize (usern, passw)
-        @username = usern
-        @password = passw
-    end
-
-    def get_username
-        @username
-    end
-end
-
-class Greeting
-    def initialize(protocol_ver, msg)
-        @protocol_version = protocol_ver
-        @message = msg
-    end
-end
-
-class ClientErrMsg
-
-    def initialize(codenr, message)
-        @code = codenr
-        @msg = message
-    end
-end
-=begin
-class PkgType 
-    [0,0,0,0] = Greet
-    [0,0,0,1] = Login,
-    [0,0,0,2] = Command,
-    [0,0,0,3] = Error,
-    [0,0,0,4] = Ok,
-    [0,0,0,5] = Response,
-    [0,0,0,6] = AccDenied,
-    [0,0,0,7] = AccGranted
-end
-=end
-
 def connect (addr, portnr, username, password)
     """ static method to get a working instance of Connection"""
     begin
         Connection.new(addr, portnr, username, password)
-    rescue Exception
+    rescue => e
+        puts e
         nil
     end
 end
 
 class Connection
+    @@command = {quit: 0, ping: 1, query: 2}
+    @@pkgtype = {greet: 0,
+                 login: 1,
+                 commands: 2,
+                 error: 3,
+                 ok: 4,
+                 response: 5,
+                 accdenied: 6,
+                 accgranted: 7 }
 
-    def initialize(addr, portnr, username, password)
-        @login = Login.new(username, password)
+    def initialize(addr, portnr, usern, passw)
+        @login = {username: usern, password: passw}
         @ip_addr = addr
         @port = portnr
         connect()
@@ -83,88 +31,82 @@ class Connection
 
     private
     def connect()
+        """ Connect to the given ip-address and port number """
         @tcp = TCPSocket.new @ip_addr, @port
-        puts "receive_greeting"
         receive_greeting
-        puts "send login"
         send_login
-        puts "receive auth status"
         receive_auth
-    end
-
-    private
-    def receive_auth
-        pkg = @tcp.recv(4)
-        puts "pkg type"
-        arr = pkg.bytes.to_a # array of bytes : pkg type
-        if arr == PkgType[ACCGRANTED]
-            puts "access granted"
-            return
-        elsif arr == PkgType[ACCDENIED]
-            puts "access denied"
-            @tcp.close()
-            return nil
-        elsif arr == PkgType[ERROR]
-            puts "error"
-            read_err
-        else
-            puts "something else"
-        end
-    end
-
-    private
-    def read_err
-        pkg = @tcp.recv(2) # ErrorKind
-        pkg = @tcp.recv(8) # msgsize
-        num = byte_array_to_int(pkg.bytes.to_a)
-        msg = @tcp.recv(num)
-        puts "errormsg: " + msg
-    end
-
-
-    private
-    def send_login
-        """ Send login package with username and password """
-        begin      
-            @tcp.send([LOGIN].pack("N"),0)
-            @tcp.send([@login.username.length].pack("Q>"), 0)
-            @tcp.send(@login.username,0)
-            @tcp.send([@login.password.length].pack("Q>"),0)
-            @tcp.send(@login.password,0)
-        rescue => err
-            puts "error: ", err
-            raise
-        end
     end
 
     private
     def receive_greeting
         """Receive the greeting package with version number and a
             message from the server """
-        begin
-            pkg = @tcp.recv(4)
-            puts "pkg type"
-            arr = pkg.bytes.to_a # array of bytes : pkg type
-            if arr != PkgType[GREET]
-                puts "unknown pkg, close connection, return nil"
-                @tcp.close()
-                return nil
-            end
-
-            vrs_nr = @tcp.recv(1)
-            
-            pkg = @tcp.recv(8)
-            num = byte_array_to_int(pkg.bytes.to_a)
-            
-            puts "msg itself"
-            msg = @tcp.recv(num)
-            puts msg
-            
-            @greet = Greeting.new vrs_nr, msg
-        rescue => e
-            puts e
-            raise 
+        
+        pkg = @tcp.recv(4)
+        arr = pkg.bytes.to_a.pack("C*").unpack("N").first
+        if arr != @@pkgtype[:greet]
+            puts "unknown pkg, close connection, return nil"
+            @tcp.close()
+            return nil
         end
+
+        # receive server versions number
+        vrs_nr = @tcp.recv(1)
+        
+        # receive size of message
+        pkg = @tcp.recv(8)
+        num = byte_array_to_int(pkg.bytes.to_a)
+        
+        # receive message
+        msg = @tcp.recv(num)
+        
+        # set the greeting message and version number
+        @greet = {version: vrs_nr.bytes.to_a.first, message: msg}
+    end
+
+    private
+    def send_login
+        """ Send login package with username and password """
+        @tcp.send([@@pkgtype[:login]].pack("N"), 0)
+        @tcp.send([@login[:username].length].pack("Q>"), 0)
+        @tcp.send(@login[:username], 0)
+        @tcp.send([@login[:password].length].pack("Q>"), 0)
+        @tcp.send(@login[:password], 0)
+    end
+
+
+    private
+    def receive_auth
+        """ Receive authentication status """
+        pkg = @tcp.recv(4)
+       
+        arr = pkg.bytes.to_a.pack("C*").unpack("N").first
+        if arr == @@pkgtype[:accgranted]
+            # do nothing
+        elsif arr == @@pkgtype[:accdenied]
+            @tcp.close()
+            @errormessage = {errorkind: @@pkgtype[:accdenied], 
+                             message: "Access denied. Close connection"}
+            raise "access denied" # throw a runtime exception
+        elsif arr == @@pkgtype[:error]
+            read_err
+            raise "Error package received." # throw a runtime exception
+        else
+            @errormessage = {errorkind: 1, 
+                             message: "Unexpected package received."}
+            raise "Unexpected package received."
+        end
+    end
+
+    private
+    def read_err
+        """ Read the package with errorkind and error message. """
+        errkind = @tcp.recv(2) # ErrorKind
+        pkg = @tcp.recv(8) # msgsize
+        num = byte_array_to_int(pkg.bytes.to_a)
+        msg = @tcp.recv(num)
+        @errormessage = {errorkind: errkind, message: msg}
     end
 
     private
@@ -179,40 +121,97 @@ class Connection
 
     public
     def ping
-        @tcp.send([COMMAND].pack("N"),0)
-        @tcp.send([PING].pack("N"),0)
-        pkg = @tcp.recv(4)
-        arr = pkg.bytes.to_a # array of bytes : pkg type
-        if arr == PkgType[OK]
-            puts "ping done"
-        elsif arr == PkgType[ERROR]
-            puts "ping failed, but connection ok"
-        else 
-            puts "unknown package"
+        """ Send ping command. 
+            Return true if ping was successful, else false. """
+        begin
+            @tcp.send([@@pkgtype[:commands]].pack("N"), 0)
+            @tcp.send([@@command[:ping]].pack("N"), 0)
+            pkg = @tcp.recv(4)
+            arr = pkg.bytes.to_a.pack("C*").unpack("N").first # array of bytes : pkg type
+            if arr == @@pkgtype[:ok]
+                true
+            elsif arr == @@pkgtype[:error]
+                read_err
+                false
+            else 
+                @errormessage = {errorkind: 1, 
+                             message: "Unexpected package received."}
+                false
+            end
+        rescue => e
+            false
         end
     end
 
     public
     def quit
-        @tcp.send([COMMAND].pack("N"),0)
-        @tcp.send([QUIT].pack("N"),0)
-        pkg = @tcp.recv(4)
-        arr = pkg.bytes.to_a # array of bytes : pkg type
-        if arr == PkgType[OK]
-            puts "close done"
-        elsif arr == PkgType[ERROR]
-            puts "close done, but with error"
-            @tcp.close
-        else 
-            puts "unknown package"
-            @tcp.close
+        """ Send quit command. Return true if quit was successful and 
+            close the connection, else false. """
+        begin 
+            @tcp.send([@@pkgtype[:commands]].pack("N"), 0)
+            @tcp.send([@@command[:quit]].pack("N"), 0)
+            pkg = @tcp.recv(4)
+            arr = pkg.bytes.to_a.pack("C*").unpack("N").first # array of bytes : pkg type
+            if arr == @@pkgtype[:ok]
+                @tcp.close
+                true
+            elsif arr == @@pkgtype[:error]
+                read_err
+                false
+            else 
+                @errormessage = {errorkind: 1, 
+                             message: "Unexpected package received."}
+                false
+            end
+        rescue => e
+            false
         end
+    end
+
+    public
+    def get_version
+        """ Return the version of the server """
+        @greet[:version]
+    end
+
+    public 
+    def get_port
+        """ Return the port number of the connection. """
+        @port
+    end
+
+    public 
+    def get_username
+        """ Return the username used to authenticate this connection. """
+        @login[:username]
+    end
+
+    public 
+    def get_greeting_msg
+        """ Return the greeting message sent by server. """
+        @greet[:message]
+    end
+
+    public 
+    def get_address
+        """ Return the ip address of the connection """
+        @ip_addr
+    end
+
+    public
+    def execute(query)
+        """ Send query to the server and receive response. Return ResultSet if
+            query was successful, else nil. """
+            # send the pkgtype
+            # @tcp.send([@@pkgtype[:commands]].pack("N"), 0)
+
+            # send the query structure
     end
 =begin
     private 
     def int_to_byte_array(length)
         """ Convert an integer value to an eight byte array """
-        array = [0,0,0,0,0,0,0,0] # default values
+        array = [0, 0, 0, 0, 0, 0, 0, 0] # default values
         for i in 0..7
             array[i] = length / 256**(7-i)
             length = length -array[i] * 256**(7-i)
@@ -220,30 +219,6 @@ class Connection
         array
     end
 =end
-    
-
-    
-=begin
-        
-        
-        # @tcp.
-        end
-        def execute()
-        def get_version()
-        def get_address()
-        def get_port()
-        def get_username()
-        def get_greeting_msg()
-=end
-end
-def int_to_byte_array(length)
-    """ Convert an integer value to an eight byte array """
-    array = [0,0,0,0,0,0,0,0] # default values
-    for i in 0..7
-        array[i] = length / 256**(7-i)
-        length = length -array[i] * 256**(7-i)
-    end
-    array
 end
 ##################### Tests ##########################################
 
@@ -251,13 +226,22 @@ end
 =end
 conn = connect("127.0.0.1", 4242, "con", "nect")
 if conn.class == Connection
-    puts "connection established"
+    puts "Versionsnr: #{conn.get_version}. Authenticated as #{conn.get_username}"
+    puts "#{conn.get_greeting_msg}"
+    
+    if conn.ping 
+        puts "ping done"
+    else 
+        puts "ping failed"
+    end
+    
+    if conn.quit
+        puts "quit successful."
+    else
+        puts "quit failed."
+    end
+
 else
     puts "connection failed"
 end
-
-conn.ping
-
-conn.ping
-conn.quit
 
